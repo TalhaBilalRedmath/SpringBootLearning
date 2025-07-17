f9a513bff8082598da909ae8c430bc35bb136277
TalhaBilalRedmath <talha.bilal.redm@gmail.com>
2025-07-17 10:37:51 +0500

    UnitTesting + DockerFile

---
+++ Unit Testing + Docker/PhoneBook/src/main/java/org/redmath/Service/ListService.java
@@ -0,0 +1,39 @@
+package org.redmath.Service;
+import java.util.NoSuchElementException;
+import org.redmath.Model.Contact;
+import org.redmath.Repository.ContactRep;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ListService {
+    private final ContactRep repo;
+
+    public ListService(ContactRep repo){
+        this.repo = repo;
+        System.out.println("Repo Done!");
+        System.out.println("Service Created");
+    }
+
+    public void updateContact(Contact obj){
+        repo.save(obj);
+    }
+
+    public void addToBook(Contact cont){
+        repo.save(cont);
+    }
+    public void deleteContact(String id){
+        int value = Integer.parseInt(id);
+        if (!repo.existsById(value)) {
+            throw new NoSuchElementException("No contact found with ID: " + value);
+        }
+        repo.deleteById(value);
+    }
+    public List<Contact> getAllContacts(){
+        return repo.findAll();
+    }
+    public void deleteAll() {
+        repo.deleteAll();
+    }
+}
