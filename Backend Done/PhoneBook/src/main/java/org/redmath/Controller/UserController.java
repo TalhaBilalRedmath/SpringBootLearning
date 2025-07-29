66f1db88b2c9737158b4956abd3e122bf685d1c2
TalhaBilalRedmath <talha.bilal.redm@gmail.com>
2025-07-29 14:29:32 +0500

    Oauth config done

---
+++ Backend Done/PhoneBook/src/main/java/org/redmath/Controller/UserController.java
@@ -0,0 +1,32 @@
+package org.redmath.Controller;
+import org.redmath.Model.User;
+import org.redmath.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private UserService us;
+
+    public UserController(UserService us){
+        this.us = us;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return us.getUsers();
+    }
+
+    @PostMapping("/users/add")
+    public void add(@RequestBody User u){
+        us.save(u);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteContact(@PathVariable String id) {
+        us.deleteUser(id);
+    }
+
+}
